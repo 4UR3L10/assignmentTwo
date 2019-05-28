05b4bb267404931af0d40213579d4e9a43387188
Aurelio <aureliomartinezdros@outlook.com>
2019-05-28 09:18:32 -0400

    Update Interface
    fix values and consts.

--- src/assignmenttwo/Constants.java
+++ src/assignmenttwo/Constants.java
@@ -2,7 +2,7 @@ package assignmenttwo;
 
 public interface Constants
 {
-
     final int WRAP_AROUND = 26;
-    final int ENCODE_SHIFT = 23;
+    final int ENCODE_SHIFT = 3;
+    final int DECODE_SHIFT = 23;
 }
